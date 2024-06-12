3b7d085e1ee49f5b08c8dcb2f175a645fd226b68
Gabriel codinhoto <codinhoto18@gmail.com>
2024-06-12 20:42:13 -0300

    Adicionado o Javascript para a pagina de resultado

---
+++ pages/resultado/resultado.js
@@ -0,0 +1,11 @@
+import { trocarTema, verifiacarTema } from "../../helpers/tema-helper.js"
+ 
+const botaoTema = document.querySelector(".tema button")
+
+const body = document.querySelector("body")
+
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
+
+verifiacarTema(body, botaoTema)

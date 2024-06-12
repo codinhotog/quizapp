c29944b8bb873b21109e4f9acde2f09a59fa584e
Gabriel codinhoto <codinhoto18@gmail.com>
2024-06-12 20:21:40 -0300

    Criação do helper responsavel por mudar o tema

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-let tema = "claro"
-
+import { trocarTema, verifiacarTema } from "./helpers/tema-helper.js"
+ 
 const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
 
 const body = document.querySelector("body")
 
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
 
-function trocarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    
-    if (tema == "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
-
-}
-
-function verifiacarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "escuro") {
-        body.classList.add("escuro")
-    }
-
-}
-
-verifiacarTema()
+verifiacarTema(body, botaoTema)
